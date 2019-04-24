e827d0b7529b10cc2766ee3e99e65cc082e72298
Lee Varnerin <lee.varnerin@gmail.com>
2019-04-24 09:03:14 -0400

    Initial Commit

---
+++ MyMemoApp-iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyMemoApp-iOS
+//
+//  Created by Lee Varnerin on 4/24/19.
+//  Copyright © 2019 Learning Mobile Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyMemoApp-iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyMemoApp-iOS
+//
+//  Created by Lee Varnerin on 4/24/19.
+//  Copyright © 2019 Learning Mobile Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
